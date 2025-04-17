4d0ddbc86cd4fc7a55ae4ec14fad2293e9dd40bb
Administrator <simonschaefer23@web.de>
2025-04-17 15:50:51 +0200

    test train split updating priors

--- scripts/prior_updating.py
+++ scripts/prior_updating.py
@@ -47,11 +47,19 @@ wide_data = pd.read_csv(parent_dir +'/data/empirical_data/experiment_data_wide.c
 empirical_data = pd.concat([narrow_data, wide_data])
 
 
-empirical_samples_narrow = dmc_helpers.fit_empirical_data(narrow_data, approximator)
+#all_parts = empirical_data['participant'].unique()
 
-empirical_samples_wide = dmc_helpers.fit_empirical_data(wide_data, approximator)
+#random_idx = np.random.choice(all_parts, size = all_parts.shape[0]//2)
 
-empirical_samples_complete = dmc_helpers.fit_empirical_data(empirical_data, approximator)
+train_idx = np.array([6361, 5281, 6214, 1108, 1538,  833, 4222,  275, 8755, 5281, 4222,
+        985, 1601, 8788,  845, 4222, 8785, 3286, 1761, 3625, 3625, 1583,
+       6844, 7768, 3754,  833, 1821, 7828,  275, 3754, 1657, 5815, 1583])
+
+empirical_samples_narrow = dmc_helpers.fit_empirical_data(narrow_data[narrow_data['participant'].isin(train_idx)], approximator)
+
+empirical_samples_wide = dmc_helpers.fit_empirical_data(wide_data[wide_data['participant'].isin(train_idx)], approximator)
+
+empirical_samples_complete = dmc_helpers.fit_empirical_data(empirical_data[empirical_data['participant'].isin(train_idx)], approximator)
 
 empirical_samples_narrow.agg(['mean', 'std'])
 
