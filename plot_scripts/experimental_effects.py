87249ffd5e72e4137e69bc5f2cbebd88a45e6d8a
Administrator <simonschaefer23@web.de>
2025-04-28 10:13:55 +0200

    update experimental effects script

--- plot_scripts/experimental_effects.py
+++ plot_scripts/experimental_effects.py
@@ -1,7 +1,7 @@
 import sys
 
-# sys.path.append("../../BayesFlow")
-# sys.path.append("../")
+sys.path.append("../../BayesFlow")
+sys.path.append("../")
 
 import os
 if "KERAS_BACKEND" not in os.environ:
@@ -15,24 +15,29 @@ import keras
 
 import bayesflow as bf
 
-sys.path.append('/Users/simonschaefer/Documents/BF-LIGHT')
+sys.path.append('/Users/simonschaefer/Documents/bf_dmc')
 from dmc import dmc_helpers
 import pandas as pd
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+parent_dir = '/home/administrator/Documents/bf_dmc'
+
+network_name = 'dmc_optimized_winsim_priors_sdr_estimated_200_795738'
 
 
-network_name = "network_0.02_0.0_2_10_16_128"
+model_specs_path = parent_dir + '/model_specs/model_specs_' + network_name + '.pickle'
+with open(model_specs_path, 'rb') as file:
+    model_specs = pickle.load(file)
 
-network_path = "../data/optuna_checkpoints/" + network_name + ".keras"
 
-# Load checkpoints
-approximator = keras.saving.load_model(network_path)
+# Load Checkpoints
+approximator = keras.saving.load_model(parent_dir + "/data/training_checkpoints/" + network_name + '.keras')
+
 
 # load narrow and wide data
-narrow_data = pd.read_csv('../data/model_data/experiment_data_narrow.csv')
-wide_data = pd.read_csv('../data/model_data/experiment_data_wide.csv')
+narrow_data = pd.read_csv(parent_dir + '/data/empirical_data/experiment_data_narrow.csv')
+wide_data = pd.read_csv(parent_dir + '/data/empirical_data/experiment_data_wide.csv')
 
 # concatenate data from both spacing conditions
 empirical_data = pd.concat([narrow_data, wide_data])
@@ -54,12 +59,17 @@ samples_complete=pd.concat((samples_wide, samples_narrow))
 parts=samples_complete["participant"].unique()
 
 
-param_names = ["A", "tau", "mu_c", "mu_r", "b"]
+param_names = model_specs['simulation_settings']['param_names']
 
 network_plot_folder = "../plots/experimental_effects/" + network_name
 
 if not os.path.exists(network_plot_folder):
     os.makedirs(network_plot_folder)
+
+hue_order = ["narrow", "wide"]
+palette = {"narrow": '#132a70', "wide": 'maroon'}
+
+#parts = [275]
 
 for i, part in enumerate(parts):
     
@@ -71,9 +81,15 @@ for i, part in enumerate(parts):
     for p, ax in zip(param_names, axes):
         
         part_data = samples_complete[samples_complete["participant"]==part]
-        part_data = part_data.reset_index(drop=True)
+        part_data = pd.DataFrame(part_data.reset_index(drop=True))
+
+        #dat = pd.DataFrame(part_data.groupby(['participant', 'spacing']).agg(['mean', 'std']))
         
-        sns.boxplot(data=part_data, ax=ax, x="spacing", y=p)
+        sns.pointplot(data=part_data, ax=ax, x="spacing", y=p, color='#132a70',errorbar="sd")
+
+        for patch in ax.patches:
+            facecolor = patch.get_facecolor()
+            patch.set_facecolor((*facecolor[:3], 0.3)) 
         ax.set_ylabel("")
 
         suff = "$\\" if p in ["tau", "mu_c", "mu_r"] else "$"
@@ -85,3 +101,57 @@ for i, part in enumerate(parts):
     fig.suptitle(str(part))    
     fig.tight_layout()
     fig.savefig(network_plot_folder + "/experimental_effects_" + network_name + str(part) + ".png")
+
+
+fig, axes = plt.subplots(1, 5, figsize=(15,3))
+
+#samples_complete_filtered = samples_complete[samples_complete["participant"].isin([275])].reset_index()
+
+for i, ax in enumerate(axes):
+
+    sns.kdeplot(data=samples_complete.reset_index(), x=param_names[i], ax=ax, hue='spacing')
+    if i < 4:
+        ax.legend_.remove()
+
+fig.tight_layout()
+
+pd.DataFrame(samples_complete.reset_index().pivot(index=['participant', 'sampling_time'], columns=['A', 'tau']))
+
+
+samples_narrow.columns = [name + '_narrow' for name in samples_narrow.columns]
+samples_wide.columns = [name + '_wide' for name in samples_wide.columns]
+
+sample_com_wide = pd.concat([samples_narrow, samples_wide], axis=1)
+
+for p in param_names:
+
+    sample_com_wide['d_'+ p] = sample_com_wide[p + '_narrow'] - sample_com_wide[p + '_wide']
+
+    sample_com_wide['d_'+ p] = sample_com_wide['d_'+ p] / np.std(sample_com_wide['d_'+ p])
+
+
+
+fig, axes = plt.subplots(1, 5, sharey=True, sharex=True, figsize=(15,3))
+
+for i, ax in enumerate(axes):
+
+    sns.kdeplot(data=sample_com_wide.reset_index(), x= 'd_' + param_names[i], ax=ax, color='#132a70', fill=True)
+
+    post_mean = np.mean(sample_com_wide['d_' + param_names[i]])
+    ax.axvline(x=post_mean, color='black', linestyle='--', linewidth=1)
+
+    suff = "$\\" if param_names[i] in ["tau", "mu_c", "mu_r"] else "$"
+
+    label = suff + param_names[i] + "$"
+
+    ax.set_title(label)
+    ax.set_xlabel('Post Samples Difference')
+ 
+
+    ax.text(post_mean + 1.1, 0.4, '$d = $' + str(round(post_mean, 2)), fontsize=12, color='black')
+
+
+fig.tight_layout()
+
+
+fig.savefig(network_plot_folder + "/experimental_effects_" + network_name + "_post_samples_difference.png")
